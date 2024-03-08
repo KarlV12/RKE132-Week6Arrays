07e8748726d218e64352037d1d82fc84229ae58e
Karl V <karlaarne.vihm1@gmail.com>
2024-03-08 08:59:15 +0200

    add week6 code

---
+++ Week6StringsAndArrays/Program.cs
@@ -0,0 +1,27 @@
+﻿string hello = " hello, world? ";
+
+int stringLength = hello.Length;
+
+//string trimmedString = hello.Trim();
+
+hello = hello.Trim();
+
+hello = hello.Replace(hello[0], char.ToUpper(hello[0].Replace('?', '!'));
+
+char firstLetter = hello[0];
+
+firstLetter = hello[0];
+
+Console.WriteLine(firstLetter);
+
+//int wordCounter = 1;
+
+//for(int i = 0; i < hello.Length; i++)
+//{
+//   if (char.IsWhiteSpace(hello[i]))
+//    {
+//        wordCounter++;
+//    }
+//}
+
+//Console.WriteLine($"Word count in {hello}: {wordCounter} words.");
